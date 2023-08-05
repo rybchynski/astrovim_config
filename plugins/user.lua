5e64835bc3947573c2ee5e73fc89f09731c96354
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2023-08-05 15:53:53 +0300

    (feat): Add es7+ react snippets plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -5,28 +5,34 @@
 -- FIX: some fix note.
 -- WARNING: some warning.
 -- TEST: some test.
-
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    -- You can also use 'opt = {}' it is the same as config = function()...
-    config = function() require("todo-comments").setup {} end,
-    event = "User AstroFile",
-    opt = {},
-    cmd = { "TodoQuickFix" },
-    keys = {
-      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
-    },
-  },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = {"nvim-lua/plenary.nvim"},
+        -- You can also use 'opt = {}' it is the same as config = function()...
+        config = function() require("todo-comments").setup {} end,
+        event = "User AstroFile",
+        opt = {},
+        cmd = {"TodoQuickFix"},
+        keys = {
+            {
+                "<leader>T",
+                "<cmd>TodoTelescope<cr>",
+                desc = "Open TODOs in Telescope"
+            }
+        }
+    }, {'projekt0n/github-nvim-theme'}, {
+        "dsznajder/vscode-es7-javascript-react-snippets",
+        run = "yarn install --frozen-lockfile && yarn compile"
+    }
 }
