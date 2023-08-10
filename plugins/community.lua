3a366d3d9970afde9c6be8dca10d22d86c902910
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2023-08-10 23:32:22 +0300

    (feat): Add new colorschemes, change default theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,17 +1,17 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+    -- Add the community repository of plugin specifications
+    "AstroNvim/astrocommunity",
+    -- example of imporing a plugin, comment out to use it or add your own
+    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.json" },
-  -- { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.colorscheme.everforest" },
-  -- { import = "astrocommunity.colorscheme.tokyonight" },
+    -- {import = "astrocommunity.colorscheme.catppuccin"},
+    -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    {import = "astrocommunity.pack.typescript"},
+    {import = "astrocommunity.pack.json"}, {import = "astrocommunity.pack.php"},
+    {import = "astrocommunity.pack.yaml"},
+    {import = "astrocommunity.pack.markdown"},
+    {import = "astrocommunity.colorscheme.tokyonight"},
+    {import = "astrocommunity.colorscheme.dracula"},
+    {import = "astrocommunity.colorscheme.iceberg"}
+    -- {import = "astrocommunity.colorscheme.github-nvim-theme", enabled = true }
 }
